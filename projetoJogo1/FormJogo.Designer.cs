30d86bf3ddd1855556c84c7f246314e7beb84cc5
Tenishi123 <tenishi1cn@gmail.com>
2019-09-13 21:10:36 -0300

    Principio do jogo

--- projetoJogo1/FormJogo.Designer.cs
+++ projetoJogo1/FormJogo.Designer.cs
@@ -28,21 +28,314 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormJogo));
+            this.pctTailandia = new System.Windows.Forms.PictureBox();
+            this.pctAustralia = new System.Windows.Forms.PictureBox();
+            this.pctJapao = new System.Windows.Forms.PictureBox();
+            this.pctChina = new System.Windows.Forms.PictureBox();
+            this.pctIndia = new System.Windows.Forms.PictureBox();
+            this.pctAfricaSul = new System.Windows.Forms.PictureBox();
+            this.pctAfricaCenter = new System.Windows.Forms.PictureBox();
+            this.pctEuropaOeste = new System.Windows.Forms.PictureBox();
+            this.pctEuropaLeste = new System.Windows.Forms.PictureBox();
+            this.pct_UnitedKingdom = new System.Windows.Forms.PictureBox();
+            this.pctCaribe = new System.Windows.Forms.PictureBox();
+            this.pct_USA_Oeste = new System.Windows.Forms.PictureBox();
+            this.pct_USA_Leste = new System.Windows.Forms.PictureBox();
+            this.pctBahia = new System.Windows.Forms.PictureBox();
+            this.pctUsuario = new System.Windows.Forms.PictureBox();
+            this.pctServidorPrincipal = new System.Windows.Forms.PictureBox();
+            this.pctSaoPaulo = new System.Windows.Forms.PictureBox();
+            this.pctFundo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctTailandia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAustralia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctJapao)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctChina)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctIndia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAfricaSul)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAfricaCenter)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctEuropaOeste)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctEuropaLeste)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_UnitedKingdom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCaribe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_USA_Oeste)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_USA_Leste)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBahia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUsuario)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctServidorPrincipal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctSaoPaulo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFundo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pctTailandia
+            // 
+            this.pctTailandia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctTailandia.Image = ((System.Drawing.Image)(resources.GetObject("pctTailandia.Image")));
+            this.pctTailandia.Location = new System.Drawing.Point(679, 314);
+            this.pctTailandia.Name = "pctTailandia";
+            this.pctTailandia.Size = new System.Drawing.Size(19, 18);
+            this.pctTailandia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctTailandia.TabIndex = 17;
+            this.pctTailandia.TabStop = false;
+            // 
+            // pctAustralia
+            // 
+            this.pctAustralia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctAustralia.Image = ((System.Drawing.Image)(resources.GetObject("pctAustralia.Image")));
+            this.pctAustralia.Location = new System.Drawing.Point(798, 387);
+            this.pctAustralia.Name = "pctAustralia";
+            this.pctAustralia.Size = new System.Drawing.Size(19, 18);
+            this.pctAustralia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctAustralia.TabIndex = 16;
+            this.pctAustralia.TabStop = false;
+            // 
+            // pctJapao
+            // 
+            this.pctJapao.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctJapao.Image = ((System.Drawing.Image)(resources.GetObject("pctJapao.Image")));
+            this.pctJapao.Location = new System.Drawing.Point(758, 166);
+            this.pctJapao.Name = "pctJapao";
+            this.pctJapao.Size = new System.Drawing.Size(19, 18);
+            this.pctJapao.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctJapao.TabIndex = 15;
+            this.pctJapao.TabStop = false;
+            // 
+            // pctChina
+            // 
+            this.pctChina.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctChina.Image = ((System.Drawing.Image)(resources.GetObject("pctChina.Image")));
+            this.pctChina.Location = new System.Drawing.Point(701, 217);
+            this.pctChina.Name = "pctChina";
+            this.pctChina.Size = new System.Drawing.Size(19, 18);
+            this.pctChina.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctChina.TabIndex = 14;
+            this.pctChina.TabStop = false;
+            // 
+            // pctIndia
+            // 
+            this.pctIndia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctIndia.Image = ((System.Drawing.Image)(resources.GetObject("pctIndia.Image")));
+            this.pctIndia.Location = new System.Drawing.Point(603, 265);
+            this.pctIndia.Name = "pctIndia";
+            this.pctIndia.Size = new System.Drawing.Size(19, 18);
+            this.pctIndia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctIndia.TabIndex = 13;
+            this.pctIndia.TabStop = false;
+            // 
+            // pctAfricaSul
+            // 
+            this.pctAfricaSul.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctAfricaSul.Image = ((System.Drawing.Image)(resources.GetObject("pctAfricaSul.Image")));
+            this.pctAfricaSul.Location = new System.Drawing.Point(450, 429);
+            this.pctAfricaSul.Name = "pctAfricaSul";
+            this.pctAfricaSul.Size = new System.Drawing.Size(19, 18);
+            this.pctAfricaSul.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctAfricaSul.TabIndex = 12;
+            this.pctAfricaSul.TabStop = false;
+            // 
+            // pctAfricaCenter
+            // 
+            this.pctAfricaCenter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctAfricaCenter.Image = ((System.Drawing.Image)(resources.GetObject("pctAfricaCenter.Image")));
+            this.pctAfricaCenter.Location = new System.Drawing.Point(401, 293);
+            this.pctAfricaCenter.Name = "pctAfricaCenter";
+            this.pctAfricaCenter.Size = new System.Drawing.Size(19, 18);
+            this.pctAfricaCenter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctAfricaCenter.TabIndex = 11;
+            this.pctAfricaCenter.TabStop = false;
+            // 
+            // pctEuropaOeste
+            // 
+            this.pctEuropaOeste.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctEuropaOeste.Image = ((System.Drawing.Image)(resources.GetObject("pctEuropaOeste.Image")));
+            this.pctEuropaOeste.Location = new System.Drawing.Point(368, 166);
+            this.pctEuropaOeste.Name = "pctEuropaOeste";
+            this.pctEuropaOeste.Size = new System.Drawing.Size(19, 18);
+            this.pctEuropaOeste.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctEuropaOeste.TabIndex = 10;
+            this.pctEuropaOeste.TabStop = false;
+            // 
+            // pctEuropaLeste
+            // 
+            this.pctEuropaLeste.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctEuropaLeste.Image = ((System.Drawing.Image)(resources.GetObject("pctEuropaLeste.Image")));
+            this.pctEuropaLeste.Location = new System.Drawing.Point(450, 166);
+            this.pctEuropaLeste.Name = "pctEuropaLeste";
+            this.pctEuropaLeste.Size = new System.Drawing.Size(19, 18);
+            this.pctEuropaLeste.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctEuropaLeste.TabIndex = 9;
+            this.pctEuropaLeste.TabStop = false;
+            // 
+            // pct_UnitedKingdom
+            // 
+            this.pct_UnitedKingdom.Image = ((System.Drawing.Image)(resources.GetObject("pct_UnitedKingdom.Image")));
+            this.pct_UnitedKingdom.Location = new System.Drawing.Point(379, 102);
+            this.pct_UnitedKingdom.Name = "pct_UnitedKingdom";
+            this.pct_UnitedKingdom.Size = new System.Drawing.Size(19, 18);
+            this.pct_UnitedKingdom.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_UnitedKingdom.TabIndex = 8;
+            this.pct_UnitedKingdom.TabStop = false;
+            // 
+            // pctCaribe
+            // 
+            this.pctCaribe.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctCaribe.Image = ((System.Drawing.Image)(resources.GetObject("pctCaribe.Image")));
+            this.pctCaribe.Location = new System.Drawing.Point(148, 265);
+            this.pctCaribe.Name = "pctCaribe";
+            this.pctCaribe.Size = new System.Drawing.Size(19, 18);
+            this.pctCaribe.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctCaribe.TabIndex = 7;
+            this.pctCaribe.TabStop = false;
+            // 
+            // pct_USA_Oeste
+            // 
+            this.pct_USA_Oeste.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pct_USA_Oeste.Image = ((System.Drawing.Image)(resources.GetObject("pct_USA_Oeste.Image")));
+            this.pct_USA_Oeste.Location = new System.Drawing.Point(68, 175);
+            this.pct_USA_Oeste.Name = "pct_USA_Oeste";
+            this.pct_USA_Oeste.Size = new System.Drawing.Size(19, 18);
+            this.pct_USA_Oeste.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_USA_Oeste.TabIndex = 6;
+            this.pct_USA_Oeste.TabStop = false;
+            // 
+            // pct_USA_Leste
+            // 
+            this.pct_USA_Leste.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pct_USA_Leste.Image = ((System.Drawing.Image)(resources.GetObject("pct_USA_Leste.Image")));
+            this.pct_USA_Leste.Location = new System.Drawing.Point(181, 217);
+            this.pct_USA_Leste.Name = "pct_USA_Leste";
+            this.pct_USA_Leste.Size = new System.Drawing.Size(19, 18);
+            this.pct_USA_Leste.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pct_USA_Leste.TabIndex = 5;
+            this.pct_USA_Leste.TabStop = false;
+            // 
+            // pctBahia
+            // 
+            this.pctBahia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctBahia.Image = ((System.Drawing.Image)(resources.GetObject("pctBahia.Image")));
+            this.pctBahia.Location = new System.Drawing.Point(290, 329);
+            this.pctBahia.Name = "pctBahia";
+            this.pctBahia.Size = new System.Drawing.Size(19, 18);
+            this.pctBahia.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctBahia.TabIndex = 4;
+            this.pctBahia.TabStop = false;
+            this.pctBahia.Click += new System.EventHandler(this.PctBahia_Click);
+            // 
+            // pctUsuario
+            // 
+            this.pctUsuario.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctUsuario.Image = ((System.Drawing.Image)(resources.GetObject("pctUsuario.Image")));
+            this.pctUsuario.Location = new System.Drawing.Point(758, 407);
+            this.pctUsuario.Name = "pctUsuario";
+            this.pctUsuario.Size = new System.Drawing.Size(34, 28);
+            this.pctUsuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctUsuario.TabIndex = 3;
+            this.pctUsuario.TabStop = false;
+            // 
+            // pctServidorPrincipal
+            // 
+            this.pctServidorPrincipal.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctServidorPrincipal.Image = ((System.Drawing.Image)(resources.GetObject("pctServidorPrincipal.Image")));
+            this.pctServidorPrincipal.Location = new System.Drawing.Point(242, 365);
+            this.pctServidorPrincipal.Name = "pctServidorPrincipal";
+            this.pctServidorPrincipal.Size = new System.Drawing.Size(30, 28);
+            this.pctServidorPrincipal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctServidorPrincipal.TabIndex = 2;
+            this.pctServidorPrincipal.TabStop = false;
+            // 
+            // pctSaoPaulo
+            // 
+            this.pctSaoPaulo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.pctSaoPaulo.Image = ((System.Drawing.Image)(resources.GetObject("pctSaoPaulo.Image")));
+            this.pctSaoPaulo.Location = new System.Drawing.Point(278, 387);
+            this.pctSaoPaulo.Name = "pctSaoPaulo";
+            this.pctSaoPaulo.Size = new System.Drawing.Size(19, 18);
+            this.pctSaoPaulo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctSaoPaulo.TabIndex = 1;
+            this.pctSaoPaulo.TabStop = false;
+            this.pctSaoPaulo.Click += new System.EventHandler(this.PctSaoPaulo_Click);
+            // 
+            // pctFundo
+            // 
+            this.pctFundo.Image = ((System.Drawing.Image)(resources.GetObject("pctFundo.Image")));
+            this.pctFundo.Location = new System.Drawing.Point(-1, -3);
+            this.pctFundo.Name = "pctFundo";
+            this.pctFundo.Size = new System.Drawing.Size(936, 526);
+            this.pctFundo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pctFundo.TabIndex = 0;
+            this.pctFundo.TabStop = false;
+            this.pctFundo.WaitOnLoad = true;
             // 
             // FormJogo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(934, 522);
             this.ControlBox = false;
+            this.Controls.Add(this.pctTailandia);
+            this.Controls.Add(this.pctAustralia);
+            this.Controls.Add(this.pctJapao);
+            this.Controls.Add(this.pctChina);
+            this.Controls.Add(this.pctIndia);
+            this.Controls.Add(this.pctAfricaSul);
+            this.Controls.Add(this.pctAfricaCenter);
+            this.Controls.Add(this.pctEuropaOeste);
+            this.Controls.Add(this.pctEuropaLeste);
+            this.Controls.Add(this.pct_UnitedKingdom);
+            this.Controls.Add(this.pctCaribe);
+            this.Controls.Add(this.pct_USA_Oeste);
+            this.Controls.Add(this.pct_USA_Leste);
+            this.Controls.Add(this.pctBahia);
+            this.Controls.Add(this.pctUsuario);
+            this.Controls.Add(this.pctServidorPrincipal);
+            this.Controls.Add(this.pctSaoPaulo);
+            this.Controls.Add(this.pctFundo);
             this.Name = "FormJogo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.FormJogo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctTailandia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAustralia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctJapao)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctChina)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctIndia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAfricaSul)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctAfricaCenter)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctEuropaOeste)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctEuropaLeste)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_UnitedKingdom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctCaribe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_USA_Oeste)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pct_USA_Leste)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBahia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctUsuario)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctServidorPrincipal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctSaoPaulo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pctFundo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pctFundo;
+        private System.Windows.Forms.PictureBox pctSaoPaulo;
+        private System.Windows.Forms.PictureBox pctServidorPrincipal;
+        private System.Windows.Forms.PictureBox pctUsuario;
+        private System.Windows.Forms.PictureBox pctBahia;
+        private System.Windows.Forms.PictureBox pct_USA_Leste;
+        private System.Windows.Forms.PictureBox pct_USA_Oeste;
+        private System.Windows.Forms.PictureBox pctCaribe;
+        private System.Windows.Forms.PictureBox pct_UnitedKingdom;
+        private System.Windows.Forms.PictureBox pctEuropaLeste;
+        private System.Windows.Forms.PictureBox pctEuropaOeste;
+        private System.Windows.Forms.PictureBox pctAfricaCenter;
+        private System.Windows.Forms.PictureBox pctAfricaSul;
+        private System.Windows.Forms.PictureBox pctIndia;
+        private System.Windows.Forms.PictureBox pctChina;
+        private System.Windows.Forms.PictureBox pctJapao;
+        private System.Windows.Forms.PictureBox pctAustralia;
+        private System.Windows.Forms.PictureBox pctTailandia;
     }
 }
--- projetoJogo1/FormJogo.cs
+++ projetoJogo1/FormJogo.cs
@@ -7,14 +7,95 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using projetoJogo1.Properties;
 
 namespace projetoJogo1
 {
     public partial class FormJogo : Form
     {
+
+        int codPonto = 0, codUltimoPonto = 0;
+        bool SaoPaulo = false /*01*/, Bahia = false /*02*/;
+
         public FormJogo()
         {
             InitializeComponent();
+        }
+
+        private void mudarPonto(PictureBox pctPonto, bool codCondicao)
+        {
+
+            if (codCondicao)
+            {
+
+                SaoPaulo = false;
+                Image SprintPonto = Properties.Resources.pontoNetIcone;
+                pctPonto.Image = SprintPonto;
+
+            }
+            else
+            {
+
+                SaoPaulo = true;
+                Image SprintPonto = Properties.Resources.pontoNetIcone2;
+                pctPonto.Image = SprintPonto;
+
+            }
+
+        }
+
+        private void gerenciarCaminho(int codCaminho)
+        {
+
+            if (codUltimoPonto == 0)
+            {
+
+                codUltimoPonto = codCaminho;
+
+            }
+            else
+            {
+
+                if(codUltimoPonto == 1 && codCaminho == 2)// conexão São paulo - Bahia
+                {
+
+                    
+
+                }
+                else
+                {
+
+                    
+
+                }
+
+            }
+            codPonto = 0;
+
+        }
+
+        private void PctBahia_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void FormJogo_Load(object sender, EventArgs e)
+        {
+
+            pctFundo.Height = this.Height;
+            pctFundo.Width = this.Width;//Ajuste de tamanho da imagem com a tela
+
+        }
+
+        private void PctSaoPaulo_Click(object sender, EventArgs e)
+        {
+
+            codPonto = 1;
+            gerenciarCaminho(codPonto);
+            mudarPonto(pctSaoPaulo, SaoPaulo);
+
         }
     }
 }
